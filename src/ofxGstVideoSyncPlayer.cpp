33cda1dc34aeb3c0f43d67aff3e2aba39133c5e1
Stephen Buchanan <stephen.buchanan@random.studio>
2017-11-03 14:22:39 +0100

    log if seek failed

--- src/ofxGstVideoSyncPlayer.cpp
+++ src/ofxGstVideoSyncPlayer.cpp
@@ -383,7 +383,10 @@ void ofxGstVideoSyncPlayer::seek(long int time_ms) {
   gint64 time_nanoseconds = time_ms * pow(10, 6);
   GstSeekFlags _flags = (GstSeekFlags) (GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_ACCURATE);
 
-  if (!gst_element_seek_simple(m_gstPipeline, GST_FORMAT_TIME, _flags, time_nanoseconds));
+  if (!gst_element_seek_simple(m_gstPipeline, GST_FORMAT_TIME, _flags, time_nanoseconds)) {
+      ofLogWarning("failed to seek");
+  }
+
 
 }
 
